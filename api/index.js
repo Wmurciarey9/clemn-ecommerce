82cb12c542c873b8eddab81a0a07da69e267d7cd
Wilson Murcia Rey <wmurciarey9@gmail.com>
2022-10-05 21:13:12 -0500

    fix errors and added proctfile file

--- api/index.js
+++ api/index.js
@@ -37,6 +37,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Backend server is running");
 });
